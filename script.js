21e2d24dd71e69d62981e7027970921899c9aec8
fahmiardiansya <pasep9059@gmail.com>
2023-12-31 03:14:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Tambahkan script.js jika Anda ingin menambahkan efek atau interaktivitas tertentu.
+
+// Contoh script animasi sederhana menggunakan jQuery (pastikan untuk menyertakan jQuery library).
+// Jika tidak ingin menggunakan jQuery, Anda dapat menggunakan JavaScript murni atau library animasi lainnya.
+
+$(document).ready(function(){
+    $(".card").hide().fadeIn(1500); // Efek fade in pada kartu ucapan
+});
